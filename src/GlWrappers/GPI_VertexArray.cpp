b716d26af611e2ad314a589f5b3cce01734675e7
Ostrikov Serhii <serostrikov@gmail.com>
2021-11-25 19:55:50 +0300

    Added OpenGL Vertex Array abstraction

---
+++ src/GlWrappers/GPI_VertexArray.cpp
@@ -0,0 +1,34 @@
+#include "GPI_VertexArray.h"
+#include "glad/glad.h"
+
+GPI_VertexArray GPI_CreateVertexArray(GPI_VertexLayout* layout, GPI_Buffer* VBO, GPI_Buffer* IBO)
+{
+    uint32_t vaoID;
+    glGenVertexArrays(1, &vaoID);
+    return {vaoID, layout, VBO, IBO};
+}
+
+void GPI_BindVertexArrayAttribs(GPI_VertexArray* target)
+{
+    glBindVertexArray(target->ID);
+    GPI_BindBuffer(target->VBO);
+    GPI_BindBuffer(target->IBO);
+    uint32_t offset = 0;
+    for(uint32_t i = 0; i < target->dataLayout->segments.size(); i++)
+    {
+        auto& current = target->dataLayout->segments[i];
+        glVertexAttribPointer(i, current.count, current.type, current.normalized, target->dataLayout->stride, (const void*)offset);
+        glEnableVertexAttribArray(i);    
+        offset += current.count * GPI_GetGlTypeSize(current.type);
+    }
+}
+
+void GPI_BindVertexArray(GPI_VertexArray* target)
+{
+    glBindVertexArray(target->ID);
+}
+
+void GPI_UnbindVertexArray(GPI_VertexArray* target)
+{
+    glBindVertexArray(0);
+}
